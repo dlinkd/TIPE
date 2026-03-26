7743ed7014098c903eedb11bb16d6d83486d122c
Link D <44369285+dlinkd@users.noreply.github.com>
2026-03-26 14:57:25 +0100

    Update reseau_neurone.py

--- reseau_neurone.py
+++ reseau_neurone.py
@@ -38,53 +38,44 @@ model = Model()
 
 
 #Couche entrée sera un tensor d'entiers on utilisera torch.IntTensor(array) pour convertir un Numpy array
-X = #tenseurs de couches d'entrées, donc les différents états avant chaque dépalcements
-y = #tenseurs de couche de sortie attendues, c'est-à-dire le bons déplacement qui est à faire
+# X: tenseurs de couches d'entrées, donc les différents états avant chaque dépalcements
+# y: tenseurs de couche de sortie attendues, c'est-à-dire le bons déplacement qui est à faire
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41) #divise entre les données qu'on va utiliser pour entrainer et celle qu'on va utiliser pour vérifier que l'entrainement donne de bons résultat
-
-# Set the criterion of model to measure the error, how far off the predictions are from the data
-criterion = nn.CrossEntropyLoss()
-# Choose Adam Optimizer, lr = learning rate (if error doesn't go down after a bunch of iterations (epochs), lower our learning rate)
-optimizer = torch.optim.Adam(model.parameters(), lr=1)
-     
-# Train our model! (Pour le moment, cet entrainement a besoin qu'on sache exactement le bon coup, et non pas seulement qu'un était mauvais)
-# Epochs? (one run thru all the training data in our network)
-epochs = 100
-losses = []
-for i in range(epochs):
-  # Go forward and get a prediction
-  y_pred = model.forward(X_train) # Get predicted results
-
-  # Measure the loss/error, gonna be high at first
-  loss = criterion(y_pred, y_train) # predicted values vs the y_train
-
-  # Keep Track of our losses
-  losses.append(loss.detach().numpy())
-
-  # print every 10 epoch
-  if i % 10 == 0:
-    print(f'Epoch: {i} and loss: {loss}')
-
-  # Do some back propagation: take the error rate of forward propagation and feed it back
-  # thru the network to fine tune the weights
-  optimizer.zero_grad()
-  loss.backward()
-  optimizer.step()
-
-
-
-
+     def feed(X,y):
+          X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41) #divise entre les données qu'on va utiliser pour entrainer et celle qu'on va utiliser pour vérifier que l'entrainement donne de bons résultat
+          
+          # Set the criterion of model to measure the error, how far off the predictions are from the data
+          criterion = nn.CrossEntropyLoss()
+          # Choose Adam Optimizer, lr = learning rate (if error doesn't go down after a bunch of iterations (epochs), lower our learning rate)
+          optimizer = torch.optim.Adam(model.parameters(), lr=1)
+               
+          # Train our model! (Pour le moment, cet entrainement a besoin qu'on sache exactement le bon coup, et non pas seulement qu'un était mauvais)
+          # Epochs? (one run thru all the training data in our network)
+          epochs = 100
+          losses = []
+          for i in range(epochs):
+            # Go forward and get a prediction
+            y_pred = model.forward(X_train) # Get predicted results
+          
+            # Measure the loss/error, gonna be high at first
+            loss = criterion(y_pred, y_train) # predicted values vs the y_train
+          
+            # Keep Track of our losses
+            losses.append(loss.detach().numpy())
+          
+            # print every 10 epoch
+            #if i % 10 == 0:
+            #  print(f'Epoch: {i} and loss: {loss}')
+          
+            # Do some back propagation: take the error rate of forward propagation and feed it back
+            # thru the network to fine tune the weights
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
 
 #Donner une prédiction à partir d'une entrée
 donnees_entrees = torch.tensor([contenu])
-     
-
-with torch.no_grad():
-  predi = (model(donnees_entrees))
-     
-
 
 
 # Save our NN Model
@@ -94,10 +85,6 @@ torch.save(model.state_dict(), 'my_really_awesome_iris_model.pt')
 # Load the Saved Model
 new_model = Model()
 new_model.load_state_dict(torch.load('my_really_awesome_iris_model.pt'))
-     
-
-<All keys matched successfully>
-
 
 # Make sure it loaded correctly
 new_model.eval()
